455b728fc9f50fa0328d493a0cf01588e94797e2
MishaFil <fme300605@yandex.ru>
2023-11-23 18:40:09 +0700

    Подключил бд

--- djangoProject5/settings.py
+++ djangoProject5/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'djangoProject5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'misha300603',
+        'HOST': 'localhost',
+        'PORT': '5432',  # По умолчанию порт для PostgreSQL
     }
 }
 
